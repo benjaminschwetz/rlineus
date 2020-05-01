44e0219abba5d7e4f420ceb5ca3d9ccbdbc29509
BJS (Benjamin Schwetz) <bjs@novozymes.com>
2020-05-02 00:00:25 +0200

    drawing .. something

--- R/device.R
+++ R/device.R
@@ -7,7 +7,8 @@ lineus_dev <- function(){
   devout::rdevice(
     rfunction = plotter_callback,
     device_name = "line_us",
-    pl = plotter
+    pl = plotter,
+    canvas = c(0,0,0,0)
   )
 }
 plotter_callback <- function(
@@ -19,6 +20,8 @@ plotter_callback <- function(
     open = .open(args, state),
     close = .close(args, state),
     onExit = .abort(args, state),
+    clip = .clip(args, state),
+    circle = .circle(args, state),
     line = .line(args, state)
   )
   state
@@ -35,10 +38,48 @@ plotter_callback <- function(
 .abort <- function(args, state){
   state
 }
-.line<- function(args, state){
+.clip <- function(args, state){
+  state$rdata$canvas <- c(
+    args$x0,
+    args$x1,
+    args$y0,
+    args$y1
+  )
   state
 }
-min_x <- 650
-max_x <- 1775
-min_y <- -1000
-max_y <- 1000
+.circle <- function(args, state){
+  state
+}
+.line<- function(args, state){
+  sv <- function(x = NULL, y = NULL, canvas = state$rdata$canvas){
+    scale_value(x, y, canvas)
+  }
+  state$rdata$pl$g01(sv(x=args$x1),sv(y=args$y1))
+  state$rdata$pl$g01(sv(x=args$x2),sv(y=args$y2))
+  state
+}
+scale_value <- function(x= NULL, y = NULL,
+               canvas,
+               area = c(
+                 min_x = 650,
+                 max_x = 1775,
+                 min_y = -1000,
+                 max_y = 1000
+               )
+){
+  if(!is.null(x)){
+   val <- x
+   range <- canvas[1:2]
+   limits <- area[1:2]
+  } else{
+    if(!is.null(y)){
+      val <- y
+      range <- canvas[3:4]
+      limits <- area[3:4]
+    } else {
+      stop("No value")
+    }
+  }
+  out <- ((limits[2]-limits[1])*(val - range[1])/(range[2]-range[1]))+limits[1]
+  return(out)
+}
