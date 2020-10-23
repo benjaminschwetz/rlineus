3e8295a63e6fd78ffe6fd4651b963b615dc344ca
Benjamin Schwetz <benjaminschwetz@posteo.net>
2020-10-23 12:29:47 +0200

    yesssssssssssssssssssssssssssssssssssss

--- R/websocket.R
+++ R/websocket.R
@@ -1,31 +1,61 @@
-LineUsPlotter <- R6::R6Class("LineUs Plotter",
-                             inherit = websocket::WebSocket,
+LineUsPlotter <- R6::R6Class("LineUsPlotter",
                              public = list(
-                               g01 = function(x=NULL,y=NULL, z=NULL) {
-                                  vec <- unlist(list(
+                               initialize = function(url = "ws://line-us.local",
+                                                     autoConnect = FALSE){
+                                 private$websocket <- websocket::WebSocket$new(url,
+                                                                               autoConnect)
+                                 private$websocket$onOpen(function(event) {
+                                   cat("Connection opened\n")
+                                 })
+                                 private$websocket$onMessage(function(event) {
+                                   self$catch_message(event$data)
+                                 })
+                                 private$websocket$onClose(function(event) {
+                                   self$clear_log()
+                                   cat("Plotter disconnected")
+                                 })
+                                 private$websocket$onError(function(event) {
+                                   cat("Client failed to connect: ", event$message, "\n")
+                                 })
+                               },
+                               catch_message = function(message){
+                                 private$pending_message <- message
+                               },
+                               retrieve_message = function(timeout=Inf){
+                                 later::run_now(timeoutSecs = timeout)
+                                 message <- private$pending_message
+                                 private$pending_message <- NULL
+                                 return(message)
+                               },
+                               g01 = function(x=NULL,y=NULL, z=NULL, async = TRUE) {
+                                 vec <- unlist(list(
                                    X = x,
                                    Y = y,
                                    Z = z
                                  ))
                                  string <- paste0(names(vec), vec, collapse = " ")
-                                 browser()
-                                 super$send(paste("G01", string))
+                                 private$websocket$send(paste("G01", string))
+                                 out <- self$retrieve_message()
+                                 return(out)
+                               },
+                               zmap = function(){
+                                 private$websocket$send("G31")
+                                 response <- self$listen()
+                                 v_z_map <- strsplit(response, ";")[[1]]
+                                 num_z_map <- as.numeric(v_z_map)
+                                 matrix(num_z_map, nrow = 2)
+                                 response
+                               },
+                               clear_log = function(){
+                                 private$log <- NULL
+                               },
+                               connect = function() {
+                                 private$websocket$connect()
                                }
+                             ),
+                             private = list(
+                               websocket = NULL,
+                               pending_message = NULL
                              )
 )
-load_plotter <- function(url = "ws://line-us.local"){
-  lineus <- LineUsPlotter$new(url)
-  lineus$onOpen(function(event) {
-    cat("Connection opened\n")
-  })
-  lineus$onMessage(function(event) {
-    cat(event$data)
-  })
-  lineus$onClose(function(event) {
-    cat("Plotter disconnected")
-  })
-  lineus$onError(function(event) {
-    cat("Client failed to connect: ", event$message, "\n")
-  })
-  return(lineus)
-}
+
